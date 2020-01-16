a540f58219d2dab406158a4e5363069c2709372d
Omar Faleh <omar@therefore.ca>
2020-01-16 00:27:23 -0500

    Add the gatsby wordpress plugin and related packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,50 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        baseUrl: "api.morscad.com",
+        protocol: "https",
+        hostingWPCOM: false,
+        useACF: false,
+        acfOptionPageIds: [],
+        auth: {
+          htaccess_user: "",
+          htaccess_pass: "",
+          htaccess_sendImmediately: false,
+          wpcom_app_clientSecret: "",
+          wpcom_app_clientId: "",
+          wpcom_user: "",
+          wpcom_pass: "",
+
+          // If you use "JWT Authentication for WP REST API" (https://wordpress.org/plugins/jwt-authentication-for-wp-rest-api/)
+          // or (https://github.com/jonathan-dejong/simple-jwt-authentication) requires jwt_base_path, path can be found in WordPress wp-api.
+          // plugin, you can specify user and password to obtain access token and use authenticated requests against WordPress REST API.
+          jwt_user: process.env.JWT_USER,
+          jwt_pass: process.env.JWT_PASSWORD,
+          jwt_base_path: "/jwt-auth/v1/token", // Default - can skip if you are using https://wordpress.org/plugins/jwt-authentication-for-wp-rest-api/
+        },
+        cookies: {},
+        verboseOutput: false,
+        perPage: 100,
+        searchAndReplaceContentUrls: {
+        },
+        concurrentRequests: 10,
+        includedRoutes: [
+          "**/categories",
+          "**/posts",
+          "**/pages",
+          "**/media",
+          "**/tags",
+          "**/taxonomies",
+          "**/users",
+        ],
+        // Blacklisted routes using glob patterns
+        excludedRoutes: [],
+        keepMediaSizes: true
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
