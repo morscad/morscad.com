46d50d20c0032d6c4ac1dc009c8f00d436a7797f
Omar Faleh <omar@therefore.ca>
2020-06-22 23:33:46 -0400

    Linting and styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,14 @@
 import React, { useContext, useEffect, useState } from "react"
-import { graphql } from "gatsby"
-import Img from "gatsby-image"
 
 import MainLayout from "../components/layouts/MainLayout"
 import HomepageNav from "../components/home/HomepageNav"
 import MainContext from "../context/MainContext"
 
 
-const IndexPage = ({ data }) => {
-  const pageTitle = "home";
+const IndexPage = () => {
   const [state, setState] = useContext(MainContext);
   const [init, setInit] = useState(false);
 
-  console.log(data);
-  const [{ title, content, wordpress_id }] = data.allWordpressPage.nodes.filter(
-    node => node.slug === pageTitle
-  )
-  /*const [{ localFile }] = data.allWordpressWpMedia.nodes.filter(
-    node => node.post === wordpress_id
-  )*/
   useEffect(() => {
     if (!init) {
       setState({ ...state, currentSection: 'home'});
@@ -28,39 +18,8 @@ const IndexPage = ({ data }) => {
   return (
       <MainLayout>
         <HomepageNav />
-        {/*<h1>{title}</h1>
-        <div dangerouslySetInnerHTML={{ __html: content }}></div>
-        <Img fluid={localFile.childImageSharp.fluid} />*/}
       </MainLayout>
   )
 }
 
 export default IndexPage
-export const pageData = graphql`
-  {
-    allWordpressPage {
-      nodes {
-        content
-        title
-        wordpress_id
-        slug
-      }
-    }
-    allWordpressSiteMetadata {
-      nodes {
-        description
-        name
-        home
-      }
-    }
-    allWordpressWpMedia {
-      nodes {
-        media_type
-        source_url
-        post
-        mime_type
-
-      }
-    }
-  }
-`
