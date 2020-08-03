84b99e790bd5323e6cf32001ef0e4736d969eac8
Omar Faleh <omar@therefore.ca>
2020-08-02 23:57:46 -0400

    Portofolio grid

--- src/components/portfolio/PortfolioGrid.js
+++ src/components/portfolio/PortfolioGrid.js
@@ -1,28 +1,31 @@
 import React, { useContext, useEffect, useState } from "react"
 import moment from "moment"
-import { isEqual, intersectionBy } from "lodash"
+import { has, isEqual, intersectionBy } from "lodash"
+import { Link } from "gatsby"
 
 import "./PortfolioGrid.scss"
 import GatsbyImage from "gatsby-image"
 import MainContext from "../../context/MainContext"
 
-const PortfolioGrid = ({ posts }) => {
+const PortfolioGrid = () => {
   const [state, setState] = useContext(MainContext)
   const [projects, setProjects] = useState([])
   useEffect(() => {
-    const p = posts.filter(project => {
-      return (
-        intersectionBy(
-          project.categories,
-          state.categories.filter(cat => cat.selected),
-          "id"
-        ).length > 0
-      )
-    })
-    if (!isEqual(p, projects)) {
-      setProjects(p)
+    if (has(state, 'posts')) {
+      const p = state.posts.filter(project => {
+        return (
+          intersectionBy(
+            project.categories,
+            state.categories.filter(cat => cat.selected),
+            "id"
+          ).length > 0
+        )
+      })
+      if (!isEqual(p, projects)) {
+        setProjects(p)
+      }
     }
-  }, [state, posts])
+  }, [state])
   return (
     <>
       {projects.length === 0 && (
@@ -32,25 +35,27 @@ const PortfolioGrid = ({ posts }) => {
         <div className={"gridContainer"}>
           {projects.map(post => {
             return (
-              <div
-                className={"portfolioItem"}
-                onMouseEnter={() => {
-                  setState({ ...state, hoveredProject: post })
-                }}
-                onMouseLeave={() => {
-                  setState({ ...state, hoveredProject: null })
-                }}
-              >
-                <div className={"itemImage"}>
-                  <GatsbyImage
-                    fluid={post.featured_media.localFile.childImageSharp.fluid}
-                  />
+                <div
+                  className={"portfolioItem"}
+                  onMouseEnter={() => {
+                    setState({ ...state, hoveredProject: post })
+                  }}
+                  onMouseLeave={() => {
+                    setState({ ...state, hoveredProject: null })
+                  }}
+                >
+              <Link to={`/portfolio/project/${post.slug}`}>
+                  <div className={"itemImage"}>
+                    <GatsbyImage
+                      fluid={post.featured_media.localFile.childImageSharp.fluid}
+                    />
+                  </div>
+                  <div className={"itemCover"}>
+                    <div>{post.title}</div>
+                    <div>{moment(post.date).year()}</div>
+                  </div>
+              </Link>
                 </div>
-                <div className={"itemCover"}>
-                  <div>{post.title}</div>
-                  <div>{moment(post.date).year()}</div>
-                </div>
-              </div>
             )
           })}
         </div>
